7ccd73ff27628e19810df5ad3f110652ce967d72
EC2 Default User <ec2-user@ip-172-31-40-140.eu-central-1.compute.internal>
2024-06-01 11:53:25 +0000

    i added changes to this file#

--- aws_vpc_file.tf
+++ aws_vpc_file.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "example" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
@@ -6,38 +6,41 @@ resource "aws_vpc" "example" {
   }
 }
 
-resource "aws_subnet" "example" {
-  vpc_id            = aws_vpc.example.id
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "tf-example"
   }
 }
 
-data "aws_ami" "amzn-linux-2023-ami" {
-  most_recent = false
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = [123456
-"]
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.amzn-linux-2023-ami.id
-  instance_type = "c6a.2xlarge"
-  subnet_id     = aws_subnet.example.id
-
-  cpu_options {
-    core_count       = 2
-    threads_per_core = 2
-  }
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
 
   tags = {
-    Name = "tf-example"
+    Name = "primary_network_interface"
   }
 }
+
+resource "aws_instance" "foo" {
+  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
+::.:wq
+
+
+
+
+ii
